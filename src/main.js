0daeec2b298f1b8d92a0890ff9f1bab80086d08c
Keith Daulton <kdaulton@gmail.com>
2019-11-13 19:39:09 -0500

    Add basic mixpanel tracking

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import mixpanel from 'mixpanel-browser';
 
 Vue.config.productionTip = false;
+
+if (process.env.VUE_APP_TRACKING !== 'false') {
+  mixpanel.init('00f8e130c9f6233dcc214d3235d272fb');
+  mixpanel.track('vue init');
+}
 
 new Vue({
   router,
