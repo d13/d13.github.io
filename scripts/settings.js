23a457b3e63f88651bc3602ac732b1d3f4068b61
Keith Daulton <kdaulton@d13design.com>
2024-02-01 23:21:56 -0500

    Adds more design tweaks (wip)

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,15 +1,22 @@
 import esbuildPluginTsc from 'esbuild-plugin-tsc';
+// import { minifyHTMLLiteralsPlugin } from 'esbuild-plugin-minify-html-literals';
 
 export function createBuildSettings(options) {
+  const plugins = [
+    esbuildPluginTsc({
+      force: true,
+    }),
+  ];
+
+  // if (options?.minify === true) {
+  //   plugins.push(minifyHTMLLiteralsPlugin());
+  // }
+
   return {
     entryPoints: ['src/main.ts'],
     outfile: 'assets/bundle.js',
     bundle: true,
-    plugins: [
-      esbuildPluginTsc({
-        force: true,
-      }),
-    ],
+    plugins: plugins,
     ...options,
   };
 }
--- src/components/life-events/life-events.ts
+++ src/components/life-events/life-events.ts
@@ -10,18 +10,39 @@ export class KdLifeEvents extends KdBaseElement {
       :host {
         display: block;
         position: relative;
-        padding-block: var(--spacing-xl);
+        padding-block: var(--size-5);
       }
 
       .timeline {
         display: block;
         position: absolute;
-        block-size: 100%;
         inline-size: 2px;
         background-color: var(--color-raw-blue-30);
-        inset-block-start: 0;
+        inset-block: var(--size-7);
         inset-inline-start: calc(var(--size-5) / 2);
         transform: translateX(-50%);
+      }
+
+      .timeline::before,
+      .timeline::after {
+        content: '';
+        display: block;
+        position: absolute;
+        inset-inline-start: 1px;
+        inline-size: var(--size--1);
+        aspect-ratio: 1;
+        border-radius: 50%;
+        background-color: var(--color-raw-blue-30);
+      }
+
+      .timeline::before {
+        inset-block-start: 0;
+        transform: translate(-50%, -50%);
+      }
+
+      .timeline::after {
+        inset-block-end: 0;
+        transform: translate(-50%, 50%);
       }
 
       .events {
