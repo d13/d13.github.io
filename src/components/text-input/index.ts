d0909b757d0fa6e41010059b38cc19e131bb21ea
Keith Daulton <kdaulton@d13design.com>
2024-11-14 23:46:06 -0500

    Fixes text-input properties

--- src/components/text-input/index.ts
+++ src/components/text-input/index.ts
@@ -1,17 +1,15 @@
 import { customElement, property, query } from 'lit/decorators.js';
-import { LitElement, html } from 'lit';
+import { html } from 'lit';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { KdFormElement } from '../base/form-element';
 
 @customElement('kd-text-input')
 export class KdTextInput extends KdFormElement {
-  static override shadowRootOptions: ShadowRootInit = {
-    ...LitElement.shadowRootOptions,
-    delegatesFocus: true,
-  };
-
   @property({ reflect: true })
-  override type: 'text' | 'url' | 'tel' | 'email' | 'password' = 'text';
+  type: 'text' | 'url' | 'tel' | 'email' | 'password' = 'text';
+
+  @query('input', true)
+  protected _control!: HTMLElement;
 
   override render() {
     return html`<input
