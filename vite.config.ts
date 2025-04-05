e411b71a2355a3a1e8de074efe57b0b36cc446f5
Keith Daulton <kdaulton@d13design.com>
2025-04-05 00:18:01 -0400

    Adds OG tags

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { ConfigEnv, defineConfig, UserConfig } from 'vite';
 import { resolve } from 'node:path';
 import license from 'rollup-plugin-license';
+// import { ViteFaviconsPlugin } from 'vite-plugin-favicon';
+import ogPlugin from 'vite-plugin-open-graph';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
@@ -8,6 +10,55 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
   console.log('isProd', isProd);
 
   const config = {
+    plugins: [
+      license({
+        thirdParty: {
+          output: {
+            file: resolve(__dirname, 'docs/assets/vendor.LICENSE.txt'),
+          },
+        },
+        sourcemap: true,
+        banner: {
+          content: '',
+          commentStyle: 'none',
+        },
+      }),
+      // https://github.com/josh-hemphill/vite-plugin-favicon?tab=readme-ov-file#advanced-usage
+      // ViteFaviconsPlugin({
+      //   logo: './assets/media/branding/logo-square.png',
+      //   inject: true,
+      //   // https://github.com/itgalaxy/favicons#usage
+      //   favicons: {
+      //     appName: 'Keith Daulton Portfolio',
+      //     appShortName: 'Keith Daulton',
+      //     appDescription: 'Keith Daulton | Design Technologist, User Experience Engineer, Technical Leader',
+      //     developerName: 'Keith Daulton',
+      //     developerURL: null,
+      //     background: '#ffffff',
+      //     theme_color: '#ffffff',
+      //     icons: {
+      //       android: true,
+      //       appleIcon: true,
+      //       appleStartup: true,
+      //       favicons: true,
+      //       firefox: true,
+      //       windows: true,
+      //       yandex: true,
+      //     },
+      //     path: '/', // Ensure paths are relative to root
+      //     manifestRelativePaths: true, // Use relative paths in manifest
+      //   },
+      //   cache: false,
+      // }),
+      // https://github.com/Lmmmmmm-bb/vite-plugin-open-graph?tab=readme-ov-file#types
+      ogPlugin({
+        basic: {
+          title: 'Keith Daulton | Design Technologist, User Experience Engineer, Technical Leader',
+          description: 'Principal front-end web developer and designer, Keith Daulton.',
+          image: '/media/social/og-image.png',
+        },
+      }),
+    ],
     build: {
       outDir: 'docs',
       minify: isProd,
@@ -17,20 +68,6 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
           main: resolve(__dirname, 'index.html'),
           404: resolve(__dirname, '404.html'),
         },
-        plugins: [
-          license({
-            thirdParty: {
-              output: {
-                file: resolve(__dirname, 'docs/assets/vendor.LICENSE.txt'),
-              },
-            },
-            sourcemap: true,
-            banner: {
-              content: '',
-              commentStyle: 'none',
-            },
-          }),
-        ],
       },
       //   cssCodeSplit: true,
       //   // rollupOptions: {
