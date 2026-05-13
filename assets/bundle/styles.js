90ce33757140894264cce1939f0791ebed5724b9
Keith Daulton <kdaulton@d13design.com>
2026-05-13 01:59:54 -0400

    chore: remove 11ty toolchain and templates

--- assets/bundle/styles.js
+++
@@ -1,2 +0,0 @@
-import{i as S}from"./chunks/chunk-YBJVVUAP.js";function o(...l){let e=[];for(let t of l)t.styleSheet&&e.push(t.styleSheet);return e}document.adoptedStyleSheets=o(S);
-/*! For license information please see styles.js.LEGAL.txt */
