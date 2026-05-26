e47d9d4cb13fbd2f85eb34ea1a078d3eeaff125a
Keith Daulton <kdaulton@d13design.com>
2026-05-25 22:24:15 -0400

    feat: new design experiment

---
+++ src/scripts/main-2026.ts
@@ -0,0 +1,10 @@
+import { litToStyleSheet } from './system/lit-to-stylesheet';
+import * as sharedStyles from './styles/shared.css';
+
+const styles = litToStyleSheet(...Object.values(sharedStyles));
+document.adoptedStyleSheets.push(...styles);
+
+// Defer custom-element registration so shared styles are adopted first.
+setTimeout(() => {
+  void import('./components/heros/hero-image');
+});
